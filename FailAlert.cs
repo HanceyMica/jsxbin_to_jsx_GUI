86416a6430aa4027d36f91fb35cd617dbf588422
HanceyMica <117522519+HanceyMica@users.noreply.github.com>
2023-08-17 22:56:30 +0800

    Modify fail / success alert

---
+++ FailAlert.cs
@@ -0,0 +1,15 @@
+﻿namespace jsxbin_to_jsx_GUI
+{
+    public partial class FailAlert : Form
+    {
+        public FailAlert()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
